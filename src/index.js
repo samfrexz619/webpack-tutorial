74124fa0248c58185ecd56ba01864030f41fe230
samfrexz619 <samfrexz69@gmail.com>
2024-01-02 11:44:56 +0100

    integrate api endpoint

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import './styles/main.scss'
 import laughing from './assets/laughing.svg'
 
 const laughImg = document.getElementById('laughImg')
-laughImg.src = laughing
+laughImg.src = laughing;
 
-console.log(generateJoke())
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
