8475ba8e16cd6fa092ae6342c9d52115168025aa
samfrexz619 <samfrexz69@gmail.com>
2024-01-02 11:27:02 +0100

    add img to template file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import generateJoke from "./generateJoke"
 import './styles/main.scss'
 import laughing from './assets/laughing.svg'
 
+const laughImg = document.getElementById('laughImg')
+laughImg.src = laughing
 
 console.log(generateJoke())
